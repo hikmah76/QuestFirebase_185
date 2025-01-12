cef8c1251670a490c224d9d4c0ad19fa31ceb459
hikmah76 <nur.hikmah190504@gmail.com>
2025-01-12 15:53:06 +0700

    Menentukan composable untuk rute DestinasiHome.

---
+++ app/src/main/java/com/example/cloud_firestore/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,41 @@
+package com.example.cloud_firestore.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cloud_firestore.ui.view.HomeScreen
+import com.example.cloud_firestore.ui.view.InsertMhsView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        // Menentukan composable untuk rute DestinasiHome.
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                }
+            )
+        }
+
+        composable(DestinasiHome.route){
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
