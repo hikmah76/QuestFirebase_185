4655a96127c9999258a03b0152c7c67ca4deb19b
hikmah76 <nur.hikmah190504@gmail.com>
2025-01-12 16:08:15 +0700

    Fungsi untuk memperbarui state berdasarkan event yang diterima

---
+++ app/src/main/java/com/example/cloud_firestore/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,111 @@
+package com.example.cloud_firestore.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cloud_firestore.model.Mahasiswa
+import com.example.cloud_firestore.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(
+    private val mhs: MahasiswaRepository
+): ViewModel(){
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+
+
+        private set
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+    // Fungsi untuk memperbarui state berdasarkan event yang diterima
+    fun updateState(mahasiswaEvent: MahasiswaEvent){
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent
+        )
+    }
+    fun validateFields(): Boolean{
+        val  event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else " NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else " Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else " Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else " Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else " Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else " Angkatan tidak boleh kosong",
+
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun insertMahasiswa(){
+        if (validateFields()){
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    mhs.insertMahasiswa(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data berhasil disimpan")
+                }catch (e: Exception){
+                    uiState = FormState.Error("Data gagal disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data tidak valid")
+        }
+    }
+    fun resetForm(){
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+    fun resetSnackBarMessage(){
+        uiState = FormState.Idle
+    }
+
+}
+
+
+sealed class FormState{
+    object Idle: FormState()
+    object Loading : FormState()
+    data class Success(val message: String): FormState()
+    data class Error(val message: String): FormState()
+}
+
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null,
+
+){
+    fun isValid(): Boolean{
+        return nim == null &&  nama == null && jenisKelamin == null && alamat == null && kelas == null &&
+                angkatan == null
+    }
+}
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
