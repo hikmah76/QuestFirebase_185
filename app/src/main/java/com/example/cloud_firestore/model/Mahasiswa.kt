0fc1035d309bd973cca57c8b829ea7593746fde9
hikmah76 <nur.hikmah190504@gmail.com>
2025-01-13 12:40:24 +0700

    data class mahasiswa

--- app/src/main/java/com/example/cloud_firestore/model/Mahasiswa.kt
+++ app/src/main/java/com/example/cloud_firestore/model/Mahasiswa.kt
@@ -1,10 +1,10 @@
 package com.example.cloud_firestore.model
-
+//data class mahasiswa
 data class Mahasiswa(
     val nim: String,
     val nama: String,
     val alamat: String,
-    val jenis_Kelamin: String,
+    val jenisKelamin: String,
     val kelas:String,
     val angkatan: String
 ){
